3b318c80a511925f9ec2d38c4ad11f747d6cb794
Alexandr Danilov <73038403+ipbel@users.noreply.github.com>
2025-02-07 16:50:46 +0300

    Update views.py

--- the_weather/weather/views.py
+++ the_weather/weather/views.py
@@ -7,33 +7,33 @@ from .models import City
 
 # Create your views here.
 def index(request):
+    
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid={}'
-
     api = 'Your Api Key from openweathermap'
 
     if request.method == 'POST':
         form = CityForm(request.POST)
-
-        r = requests.get(url.format(request.POST.get('name'))).json()
-        if len(r) > 2:
+        response = requests.get(url.format(request.POST.get('name'))).json()
+        if len(response) > 2:
             form.save()
         else:
             form.add_error(None, "Can't find out such a city")
 
     form = CityForm()
-
     cities = City.objects.all()
-
     weather_data = []
 
     for city in cities:
-        r = requests.get(url.format(city)).json()
-
+        response = requests.get(url.format(city)).json()
+        city_temperature = response['main']['temp']
+        city_description = response['weather'][0]['description'][:1] + r['weather'][0]['description'][1:]
+        city_icon = response['weather'][0]['icon']
+        
         city_weather = {
             'city': city.name,
-            'temperature': r['main']['temp'],
-            'description': r['weather'][0]['description'][:1].upper() + r['weather'][0]['description'][1:],
-            'icon': r['weather'][0]['icon'],
+            'temperature': city_temperature,
+            'description': city_description,
+            'icon': city_icon,
         }
 
         weather_data.append(city_weather)
